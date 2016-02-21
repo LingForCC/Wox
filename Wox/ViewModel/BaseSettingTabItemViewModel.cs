3b091b9fcbde053f9de264c2997a4d262024367c
Colin Liu <colin198702@gmail.com>
2016-02-21 11:30:07 +0800

    Setup base classes for Setting ViewModel

---
+++ Wox/ViewModel/BaseSettingTabItemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wox.Core.Resource;
+
+namespace Wox.ViewModel
+{
+    public abstract class BaseSettingTabItemViewModel : BaseViewModel
+    {
+
+        #region ViewModel Properties
+
+        public string Header
+        {
+            get { return InternationalizationManager.Instance.GetTranslation(GetHeaderResourceKey()); }
+        }
+
+        public BaseViewModel Content
+        {
+            get { return GetContent(); }
+        }
+
+        #endregion
+
+        #region Abstract Methods
+
+        protected abstract string GetHeaderResourceKey();
+
+        protected abstract BaseViewModel GetContent();
+
+        #endregion
+    }
+}
---
+++ Wox/ViewModel/SettingViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wox.ViewModel
+{
+    public class SettingViewModel : BaseViewModel
+    {
+
+        #region Constructor
+
+        public SettingViewModel()
+        {
+            this.Tabs = new List<BaseSettingTabItemViewModel>();
+
+        }
+
+        #endregion
+
+        #region ViewModel Properties
+
+        public IEnumerable<BaseSettingTabItemViewModel> Tabs { get; private set; } 
+
+        #endregion
+
+    }
+}
